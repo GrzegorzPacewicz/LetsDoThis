c0ac20bc8a2a313ac4ead498c7d1749cd178b214
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-13 13:48:00 +0100

    H1, h2 added. Header added. Container added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-
 function App() {
-   return (
-    <div className="flex justify-center items-center h-screen">Hello world</div>
-  )
+    return (
+        <div className="bg-white py-8 px-6 rounded-3xl max-w-3xl mt-8 mx-2">
+
+            <header className="flex items-center justify-between gap-24">
+                <div>
+                    <h1 className="font-bold text-4xl">Do zrobienia</h1>
+                    <h2 className="font-bold text-3xl py-2">5 zadań</h2>
+                </div>
+
+                <button className="bg-blue-400 border-0 rounded-full w-12 h-12 text-3xl text-white cursor-pointer">+</button>
+
+
+            </header>
+            <div>coś Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis culpa cumque debitis dicta esse et, ex libero perferendis quae quod reiciendis repudiandae rerum saepe, temporibus tenetur velit voluptatum. Molestias?
+            </div>
+
+        </div>
+    )
 }
 
 export default App
