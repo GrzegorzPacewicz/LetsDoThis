38e327939fefcc9fc44081c68272ab199cf72364
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-14 15:44:17 +0100

    Form.jsx styles added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Form } from "./components/Form.jsx";
 
 function App() {
     return (
-        <div className="bg-white py-8 px-6 rounded-3xl max-w-3xl mt-8 mx-2 min-w-fit">
+        <div className="bg-white py-8 px-6 rounded-3xl max-w-3xl mt-8 mx-2 w-full">
 
             <header className="flex items-center justify-between gap-40 ">
+
                 <div className="">
                     <h1 className="font-bold text-4xl">Do zrobienia</h1>
                     <h2 className="font-bold text-3xl py-2">5 zadań</h2>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Form() {
     return (
-        <form className="gap-2 flex" >
-            <input type="text" className="border-[1px] px-2 border-black"/>
-            <button className="border-[1px] border-black px-2">Dodaj</button>
+        <form className="flex gap-3 mt-6" >
+            <input type="text" className="w-full border-[1px] border-blue-400 rounded p-2 border-solid"/>
+            <button className="border-[1px] rounded border-blue-400 w-max text-l px-2">Dodaj</button>
         </form>
     );
 }
