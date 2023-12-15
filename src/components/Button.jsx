c37dc3c9ec0bf615c64c781c9b44bfbe9a24df56
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-15 23:45:59 +0100

    Task.jsx styled

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Button( {textButton}) {
     return (
-        <button className="border-[1px] rounded border-current w-max text-l px-2">{textButton}</button>
+        <button className="border-[1px] rounded border-current border-blue-400 w-max px-3 py-1 text-blue-500 font-medium">{textButton}</button>
     );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,11 +3,11 @@ import { Button } from "./Button.jsx";
 
 export function Task() {
     return (
-        <div className="flex justify-between gap-4">
-            <div>Zadanie do zrobienia</div>
-            <div className="color-orange-400 flex gap-2">
-                <Button className="border-orange-400" textButton={"Zrobione"}/>
-                <Button className="border-orange-400" textButton={"Usuń zadanie"}/>
+        <div className="flex justify-between gap-4 mt-6 items-center">
+            <div className="font-medium text-l">Zadanie do zrobienia</div>
+            <div className="flex gap-2">
+                <Button textButton={"Zrobione"}/>
+                <Button textButton={"Usuń zadanie"}/>
 
             </div>
 
