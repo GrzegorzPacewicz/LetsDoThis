4870fbecd18bb32311892b05e0e000a8b95dfaa9
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-19 13:51:14 +0100

    Hover added, onClick prop added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export function Button( {textButton}) {
-    return (
-        <button className="border-[1px] rounded border-current border-blue-400 w-max px-3 py-1 text-blue-500 font-medium">{textButton}</button>
-    );
+export function Button({ textButton, onClick }) {
+  return (
+    <button
+      className="border-[1px] rounded border-blue-400 w-max px-3 py-1 text-blue-500 font-medium hover:text-white hover:bg-blue-500 transition-all duration-300 "
+      onClick={onClick}
+    >
+      {textButton}
+    </button>
+  );
 }
