0b7158d0057659d437c7fea8231b5738777a0a8c
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-29 13:20:25 +0100

    tasksReducer added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,82 @@
 import { Form } from "./components/Form.jsx";
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { getTasksNumber } from "./utils/getTasksNumber.js";
 import { Task } from "./components/Task.jsx";
 
-function App() {
-    const [tasks, setTasks] = useState([
-        { task: "Zapłacić rachunki", done: false, id: 1 },
-        { task: "Wyrzucić śmieci", done: true, id: 2 },
-    ]);
-    const [isFormShown, setIsFormShown] = useState(false);
-
-    const handleTaskSubmit = (task) => {
-        setTasks((prevTasks) => {
-            return [...prevTasks, { id: Math.random(), task, done: false }];
-        });
-        setIsFormShown(false);
-           };
-
-    function handleShowForm() {
-        setIsFormShown(true);
-    }
-
-    const handleToggleDone = (taskId) => {
-        setTasks((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === taskId ? { ...task, done: !task.done } : task
-            )
-        );
-    };
-
-    const handleDeleteTask = (taskId) => {
-        setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-    };
-
-    const tasksElements = tasks.map((task) => (
-        <Task
-            key={task.id}
-            task={task}
-            onToggleDone={handleToggleDone}
-            onDelete={handleDeleteTask}
-        />
-    ));
-
-    return (
-        <div className="bg-white py-8 px-6 rounded-3xl w-[600px]  mt-8 mx-2">
-            <header className="flex items-center justify-between gap-40 ">
-                <div className="">
-                    <h1 className="font-bold text-4xl">Do zrobienia</h1>
-                    <h2 className="font-bold text-3xl py-2">
-                        {getTasksNumber(tasks)}
-                    </h2>
-                </div>
-
-                <div className="w-32 flex justify-end">
-                    {!isFormShown && (
-                        <button
-                            className="bg-blue-400 border-0 rounded-full w-12 h-12 text-3xl text-white cursor-pointer transition-all duration-300 hover:bg-blue-700"
-                            onClick={handleShowForm}
-                        >
-                            +
-                        </button>
-                    )}
-                </div>
-            </header>
-
-            <div className="h-14 mt-8">
-                {isFormShown && <Form onTaskSubmit={handleTaskSubmit}/>}
-            </div>
-
-            <div>{tasksElements}</div>
-
-        </div>
+function tasksReducer(state, action) {
+  if (action.type === "delete") {
+    return state.filter((task) => task.id !== action.id);
+  }
+  if (action.type === "finish") {
+    return state.map((task) =>
+      task.id === action.id ? { ...task, done: !task.done } : task
     );
+  }
+}
+
+function App() {
+  const [isFormShown, setIsFormShown] = useState(false);
+
+  const [tasks, dispatch] = useReducer(tasksReducer, [
+    { task: "Zapłacić rachunki", done: false, id: 1 },
+    { task: "Wyrzucić śmieci", done: true, id: 2 },
+  ]);
+
+  const handleTaskSubmit = (task) => {
+    setTasks((prevTasks) => {
+      return [...prevTasks, { id: Math.random(), task, done: false }];
+    });
+    setIsFormShown(false);
+  };
+
+  function handleShowForm() {
+    setIsFormShown(true);
+  }
+
+  const handleToggleDone = (id) => {
+    dispatch({ type: "finish", id });
+  };
+
+  const handleDeleteTask = (id) => {
+    dispatch({ type: "delete", id });
+  };
+
+  const tasksElements = tasks.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      onToggleDone={handleToggleDone}
+      onDelete={handleDeleteTask}
+    />
+  ));
+
+  return (
+    <div className="bg-white py-8 px-6 rounded-3xl w-[600px]  mt-8 mx-2">
+      <header className="flex items-center justify-between gap-40 ">
+        <div className="">
+          <h1 className="font-bold text-4xl">Do zrobienia</h1>
+          <h2 className="font-bold text-3xl py-2">{getTasksNumber(tasks)}</h2>
+        </div>
+
+        <div className="w-32 flex justify-end">
+          {!isFormShown && (
+            <button
+              className="bg-blue-400 border-0 rounded-full w-12 h-12 text-3xl text-white cursor-pointer transition-all duration-300 hover:bg-blue-700"
+              onClick={handleShowForm}
+            >
+              +
+            </button>
+          )}
+        </div>
+      </header>
+
+      <div className="h-14 mt-8">
+        {isFormShown && <Form onTaskSubmit={handleTaskSubmit} />}
+      </div>
+
+      <div>{tasksElements}</div>
+    </div>
+  );
 }
 
 export default App;
